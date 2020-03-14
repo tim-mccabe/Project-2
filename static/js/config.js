f7b630c51d101eb93785a86d265c78b1cfc3b169
Tim McCabe <timmccabe44@gmail.com>
2020-03-14 10:38:13 -0400

    update

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,5 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoic2pnaWF1cXVlIiwiYSI6ImNrN3JuYmx4YTA2cTUzZGt4cHVpazBxNHEifQ.2ZRciQ6xa9aLrNRbDufPvA";
+const API_KEY = "pk.eyJ1IjoidGltLW1jY2FiZSIsImEiOiJjazcyaTN4eDEwMmlyM2ZvbnZqOG83aDB0In0.kNW_LjWWN6z5BAmswfVUVg";
 
 
 
